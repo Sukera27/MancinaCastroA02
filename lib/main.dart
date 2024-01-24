17e98e7a0bd2c77f9c26877f09da9570999df989
Mara2345 <rafa_mancina@hotmail.com>
2024-01-24 19:16:25 +0100

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mancinacastroa02/widgets/btn_class.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,103 +8,84 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF353434),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'CONTADOR',
-                style: TextStyle(
-                  fontFamily: 'PoppinsBold',
-                  color: Colors.white,
-                  fontSize: 64,
-                  fontWeight: FontWeight.bold,
-                ),
+    return MaterialApp(home: Counter());
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  CounterState createState() => CounterState();
+}
+
+class CounterState extends State<Counter> {
+  int count = 0;
+  void add() {
+    setState(() {
+      count++;
+    });
+  }
+
+  void subtract() {
+    setState(() {
+      if (count > 0) {
+        count--;
+      }
+    });
+  }
+
+  void resetCounter() {
+    setState(() {
+      count = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF353434),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'CONTADOR',
+              style: TextStyle(
+                fontFamily: 'PoppinsBold',
+                color: Colors.white,
+                fontSize: 64,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 150),
-              const Text(
-                '0',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                  fontSize: 200,
-                ),
+            ),
+            const SizedBox(height: 150),
+            Text(
+              "${count}",
+              style: const TextStyle(
+                fontFamily: 'Poppins',
+                color: Colors.white,
+                fontSize: 200,
               ),
-              const SizedBox(height: 100),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 120,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF1E1E1E),
-                      borderRadius: BorderRadius.circular(17),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        '+',
-                        style: TextStyle(
-                          fontFamily: 'PoppinsBold',
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 5),
-                  Container(
-                    width: 120,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF201F1F),
-                      borderRadius: BorderRadius.circular(17),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'RESET',
-                        style: TextStyle(
-                          fontFamily: 'PoppinsBold',
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 5),
-                  Container(
-                    width: 120,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF1E1D1D),
-                      borderRadius: BorderRadius.circular(17),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        '-',
-                        style: TextStyle(
-                          fontFamily: 'PoppinsBold',
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+            ),
+            const SizedBox(height: 100),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ButtonCount(text: "-", fontsize: 20, onPressed: subtract),
+                const SizedBox(width: 5),
+                ButtonCount(
+                    text: "RESET", fontsize: 20, onPressed: resetCounter),
+                const SizedBox(width: 5),
+                ButtonCount(text: "+", fontsize: 20, onPressed: add),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Text(
+              'Bienvenido Rafael_Mancina',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
               ),
-              const SizedBox(height: 50),
-              const Text(
-                'Bienvenido Rafael_Mancina',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
